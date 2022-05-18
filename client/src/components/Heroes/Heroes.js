c3204e3b4840aa9691dd43716909d182f749b9ae
yurii.mysko <yurii.mysko@userway.org>
2022-05-18 10:38:24 +0300

    remove unnecessary get

--- client/src/components/Heroes/Heroes.js
+++ client/src/components/Heroes/Heroes.js
@@ -4,27 +4,17 @@ import {CardGroup} from 'react-bootstrap'
 import Hero from './Hero/Hero';
 import PaginationButtons from "../Pagination/PaginationButtons";
 import {fetchFilteredHeroes} from "../../actions/heroes";
-import {SET_NUMBER_OF_PAGES} from "../../constants/actionTypes";
 
 const Heroes = () => {
     const dispatch = useDispatch();
     const heroes = useSelector((state) => state.heroes);
     const page = useSelector((state) => state.page);
-    const numberOfPages = useSelector((state) => state.numberOfPages);
-
-    const heroesLength = useSelector(state => state.heroes.length);
 
     useEffect(() => {
         dispatch(fetchFilteredHeroes(page));
-    }, [page, numberOfPages]);
+    }, [page]);
 
 
-    useEffect(() => {
-        dispatch({
-            type: SET_NUMBER_OF_PAGES,
-            payload: Math.ceil(heroesLength / 5),
-        });
-    }, [heroesLength]);
 
     return (<>
             <CardGroup className="d-flex justify-content-center mt-5 mb-3 flex-wrap">
